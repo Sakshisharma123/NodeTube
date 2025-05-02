a0af51ac5cec229241f6f40d31103971a3595870
Sakshishrma123 <sakshisharma.dsvv@gmail.com>
2025-05-02 22:21:17 +0530

    define routes and controller for users

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,8 @@
+const ApiError = require("../utils/ApiError");
+const ApiResponse = require("../utils/ApiResponse.");
+const asyncHandler = require("../utils/asyncHandler");
+
+
+const registerUser = {}
+
+export {registerUser};
